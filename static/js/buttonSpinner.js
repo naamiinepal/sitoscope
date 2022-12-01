a7bfd9fe6355ae3f7df10cde3bdea4898cac42e2
Safal Thapaliya <safalthapaliya@gmail.com>
2022-12-01 15:34:48 +0545

    disable site on button click

--- static/js/buttonSpinner.js
+++ static/js/buttonSpinner.js
@@ -1,7 +1,9 @@
 "use strict";
 const form = document.getElementById("form");
+const body_container = document.getElementById("bodyContainer");
 form.onsubmit = () => {
     const submit_button = document.getElementById("submit-btn");
     submit_button.disabled = true;
     submit_button.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Submitting...`
+    body_container.classList.add("disabled");
 }
