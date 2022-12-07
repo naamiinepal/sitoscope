7c3ca30c4336accebe134acb0dd040c628249ddc
Safal Thapaliya <safalthapaliya@gmail.com>
2022-12-07 15:03:00 +0545

    staticfiles

--- parasite/urls.py
+++ parasite/urls.py
@@ -27,6 +27,4 @@ urlpatterns = (
         path("api-auth/", include("rest_framework.urls")),
         path("api/address/", include("address.urls")),
     ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,4 @@
 set -e
 . .venv/bin/activate
 echo "Starting Django..."
-python manage.py runserver 0.0.0.0:23232
+gunicorn
