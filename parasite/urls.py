6582bf592a7a251764eeedd3484d2c3fa277a861
Safal Thapaliya <safalthapaliya@gmail.com>
2022-12-26 17:14:42 +0545

    fix robots filename

--- parasite/urls.py
+++ parasite/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
@@ -27,8 +25,6 @@ urlpatterns = [
     path("api/address/", include("address.urls")),
     path(
         "robots.txt",
-        TemplateView.as_view(
-            template_name="main/robots.txt", content_type="text/plain"
-        ),
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
 ]
